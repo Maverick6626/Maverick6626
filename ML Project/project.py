cc0f02f6057e369f8c533b33937d8f7ce241b872
Maverick6626 <137084874+Maverick6626@users.noreply.github.com>
2023-09-19 20:26:39 +0530

    Add files via upload

---
+++ ML Project/project.py
@@ -0,0 +1,21 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+cols = ["fLength","fWidth","fSize","fConc","fConc1","fAsym","fM3Long","fM3Trans","fAlpha","fDist","class"]
+df = pd.read_csv("magic04.data",names=cols)
+df.head()
+
+df["class"] = (df["class"] == "g").astype(int)
+df.head()
+
+for label in cols[:-1]:
+  plt.hist(df[df["class"]==1][label],color="blue", label = 'gamma',alpha=0.7,density=True)
+  plt.hist(df[df["class"]==0][label],color="red", label = 'hadron',alpha=0.7,density=True)
+  plt.title(label)
+  plt.ylabel("Probability")
+  plt.xlabel(label)
+  plt.legend()
+  plt.show()
+  
+  #Train, Valid, Test Dataset
